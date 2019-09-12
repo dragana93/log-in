bab4d6e1335d59f56995ed446f06c9fde5f1aa47
draganasavanovic <dragana.savanovic@wayseven.com>
2019-09-12 16:23:11 +0200

    added username and password

--- src/app/log-in/log-in.module.ts
+++ src/app/log-in/log-in.module.ts
@@ -4,6 +4,9 @@ import { MatMenuModule } from "@angular/material/menu";
 import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSelectModule } from "@angular/material/select";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
   declarations: [],
@@ -12,13 +15,19 @@ import { MatButtonToggleModule } from "@angular/material/button-toggle";
     MatMenuModule,
     MatButtonModule,
     MatToolbarModule,
-    MatButtonToggleModule
+    MatButtonToggleModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule
   ],
   exports: [
     MatMenuModule,
     MatButtonModule,
     MatToolbarModule,
-    MatButtonToggleModule
+    MatButtonToggleModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule
   ]
 })
 export class LogInModule {}
